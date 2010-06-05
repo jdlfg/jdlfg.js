3994c76ad670909552a19668ae547db029a94966
G. Jose De La Fuente <guillermodlf@mail.com>
2010-06-04 22:22:18 -0400

    first commit

---
+++ jdlfg.js
@@ -0,0 +1,111 @@
+{
+     version: 1,
+     homepage: "http://twitter.com/jdlfg",
+     roms:
+     [
+     {
+	name: "800MHz Kernel For Any ROM",
+	summary: "5 Slot, Low Voltage, 400MHz-800MHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg4x5x800lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg4x5x800lv.zip",
+	]
+     },
+     {
+	name: "900MHz Kernel For Any ROM",
+	summary: "5 Slot, Low Voltage, 400MHz-900MHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg4x5x900lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg4x5x900lv.zip",
+	]
+     },
+     {
+	name: "1GHz Kernel For Any ROM",
+	summary: "5 Slot, Low Voltage, 400MHz-1GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg4x5x1000lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg4x5x1000lv.zip",
+	]
+     },
+     {
+	name: "1.2GHz Kernel For Any ROM",
+	summary: "5 Slot, Low Voltage, 400MHz-1.2GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg4x5x1200lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg4x5x1200lv.zip",
+	]
+     },
+     {
+	name: "1.3GHz Kernel For Any ROM",
+	summary: "5 Slot, Low Voltage, 400MHz-1.3GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg1300.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg1300.zip",
+	]
+      },
+      {
+	name: "800MHz Kernel For Any ROM",
+	summary: "7 Slot, Low Voltage, 300MHz-800MHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg7x3x800lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg_7slot/jdlfg7x3x800lv.zip",
+	]
+      },
+      {
+	name: "1GHz Kernel For Any ROM",
+	summary: "7 Slot, Low Voltage, 300MHz-1GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg7x3x1000lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg_7slot/jdlfg7x3x1000lv.zip",
+	]
+      },
+      {
+	name: "1GHz Kernel For Any ROM",
+	summary: "7 Slot, Standard Voltage, 250MHz-1GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg250x7x1000sv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg_7slot/jdlfg250x7x1000sv.zip",
+	]
+      },
+      {
+	name: "1.1GHz Kernel For Any ROM",
+	summary: "7 Slot, Low Voltage, 400MHz-1.1GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg4x7x1100lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg_7slot/jdlfg4x7x1100lv.zip",
+	]
+     },
+     {
+	name: "1.2GHz Kernel For Any ROM",
+	summary: "7 Slot, Low Voltage, 250MHz-1.2GHz",
+        device: "sholes",
+        urls:
+	[
+	"http://dl.dropbox.com/u/6011176/jdlfg250x7x1200lv.zip",
+	"http://dl.dropbox.com/u/6744030/jdlfg_kernels/jdlfg_7slot/jdlfg250x7x1200lv.zip",
+	]
+      },
+      }	
+      ]
+}
+   
+      
+
